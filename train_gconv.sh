ed489b3de6668e364b8cbb451f49f3a9ba40112d
SitaroShinagawa <hirameki88888@gmail.com>
2018-10-13 11:58:40 +0900

    add train file

---
+++ train_gconv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=1
+
+python train.py --root "/project/nakamura-lab07/Work/seitaro-s/place2" \
+                --mask_root "./masks/256" \
+                --batch_size 16 \
+                --n_threads 16 \
+                --image_size 256 \
+				--save_dir "./snapshots/gconv" \
+				--log_dir "./logs/gconv" \
